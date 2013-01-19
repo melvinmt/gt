f6773d60bb5ed1203cc4932e9c4e95b05a44e4b9
Melvin Tercan <mmtercan@gmail.com>
2013-01-19 03:45:12 -0800

    cleaning up

--- gt.go
+++ gt.go
@@ -7,27 +7,6 @@ import (
 	"strings"
 )
 
-// var g = &gt.Build{
-// 	Index: .gt.Keys{
-// 		"homepage-greeting": gt.Strings{
-// 			"en": "Welcome to %stitle, %s#name!"
-// 			"es-LA": "¡Bienvenido a %s#title, %s#titlename!",
-// 			"nl": "Welkom bij %s#title, %s#name!",
-// 			"tr": "%s#name, %s#title'ya hoşgeldiniz!", // tag notation proves useful in SOV languages where subject comes before object/verb
-// 			"zh-CN": "欢迎%s#title, #name!", 
-// 		},
-// 	}, 
-// 	Origin: "en",
-// }
-
-// t.Target = "es"
-
-// str := g.T("homepage-greeting")
-
-// t.Target = "nl"
-
-// str = g.T("homepage-greeting")
-
 type Strings map[string]string
 type Keys map[string]Strings
 
@@ -36,25 +15,6 @@ type Build struct {
 	Target string // the target env
 	Index  Keys   // pointer to a Keys map
 }
-
-// Example:
-//     fmt.Printf(t.S("Welcome to %s#title, %s!#name!"), "gotr", "Melvin")
-//     // outputs: "¡Bienvenido a gotr, Melvin!"
-//     
-// You can add meta data to the printf verbs by using the tag notation. Not only will translators
-// understand more about the context that they're translating in, but it also prevents order issues 
-// in certain languages. (The returning string is always correctly parsed).
-// 
-// Tags are recommended but you can also use regular printf strings. But then the
-// order can be wrong in certain SOV languages (Turkish, Japanese):
-//
-//     fmt.Printf(t.S("Welcome to %s, %s"), "gotr", "Melvin")
-//     // outputs: "gotr, Melvin'ya hoşgeldiniz!" in Turkish. 
-//     // This roughly translates to: "Welcome to Melvin, gotr!" which is not what you want!
-//     
-// Tags are normally ended by a single space but you can also end tags with a plus sign:
-// "somethingbefore%s#name+somethingafter" 
-// 
 
 // Translate finds a string for key in target env and transliterates it.
 // Will return key if string or target env is not found.
@@ -97,47 +57,54 @@ func (b *Build) Translate(key string, a ...interface{}) (t string, err error) {
 
 	if len(a) == 0 { // no arguments? return string
 		return t, err
-	} else {
+	}
 
-		oVerbs, err := ParseStr(o)
+	oVerbs, err := ParseStr(o)
+
+	if err != nil {
+		return t, errors.New("Couldn't parse origin string")
+	}
+
+	tVerbs, err := ParseStr(t)
+
+	if err != nil {
+		return t, errors.New("Couldn't parse target string")
+	}
+
+	var cleanVerb string
+
+	// time to switch it up!
+	if len(oVerbs) == len(tVerbs) { // check if both verbs arrays are the same length
+		var r *regexp.Regexp
+		r, err = regexp.Compile(`(#[\w0-9-_]+)`) // compile regex to match tags
+
 		if err != nil {
-			return t, errors.New("Couldn't parse origin string")
-		}
-		tVerbs, err := ParseStr(t)
-		if err != nil {
-			return t, errors.New("Couldn't parse target string")
+			return t, errors.New("Couldn't compile regex to match tags")
 		}
 
-		var cleanVerb string
+		newArgs := make([]interface{}, len(oVerbs)) // create new args slice
 
-		// time to switch it up!
-		if len(oVerbs) == len(tVerbs) { // check if both verbs arrays are the same length
-			var r *regexp.Regexp
-			r, err = regexp.Compile(`(#[\w0-9-_]+)`) // compile regex to match tags
-			if err != nil {
-				return t, errors.New("Couldn't compile regex to match tags")
-			}
-			newArgs := make([]interface{}, len(oVerbs)) // create new args slice
-			for ti, dirtyVerb := range tVerbs {
-				for oi, ov := range oVerbs {
-					if ov == dirtyVerb && a[oi] != nil { // find original argument 
-						newArgs[ti] = a[oi]                                  // assign argument to current position
-						a[oi] = nil                                          // unset arg
-						cleanVerb = r.ReplaceAllLiteralString(dirtyVerb, "") // remove tags
-						strings.Replace(t, dirtyVerb, cleanVerb, -1)         // replace dirty verbs with clean verbs
-					}
+		for ti, dirtyVerb := range tVerbs {
+
+			for oi, ov := range oVerbs {
+
+				if ov == dirtyVerb && a[oi] != nil { // find original argument 
+					newArgs[ti] = a[oi]                                  // assign argument to current position
+					a[oi] = nil                                          // unset arg
+					cleanVerb = r.ReplaceAllLiteralString(dirtyVerb, "") // remove tags
+					strings.Replace(t, dirtyVerb, cleanVerb, -1)         // replace dirty verbs with clean verbs
 				}
 			}
-			if len(newArgs) == len(a) {
-				t = fmt.Sprintf(t, newArgs...) // replace verbs with args
-			} else {
-				return t, errors.New("Impossible to assign arguments to string.")
-			}
-
-		} else {
-			return t, errors.New("Number of printf verbs in origin and target string do not match.")
 		}
 
+		if len(newArgs) == len(a) {
+			t = fmt.Sprintf(t, newArgs...) // replace verbs with args
+		} else {
+			return t, errors.New("Impossible to assign arguments to string.")
+		}
+
+	} else {
+		return t, errors.New("Number of printf verbs in origin and target string do not match.")
 	}
 
 	return t, err
@@ -149,8 +116,10 @@ func (b *Build) T(key string, a ...interface{}) (t string) {
 	return t
 }
 
+// ParseStr returns an array of parsed verbs with optional tags
 func ParseStr(str string) (verbs []string, err error) {
 	r, err := regexp.Compile(`(%(?:\d+\$)?[+-]?(?:[ 0]|'.{1})?-?\d*(?:\.\d+)?#?[bcdeEfFgGopqstTuUvxX%]?)(#[\w0-9-_]+)?`)
+
 	if err != nil {
 		return verbs, errors.New("Couldn't compile regex to parse strings")
 	}
@@ -158,7 +127,6 @@ func ParseStr(str string) (verbs []string, err error) {
 
 	if len(m) > 0 {
 		verbs = make([]string, len(m))
-
 		for i, v := range m[0] {
 			verbs[i] = v
 		}
@@ -169,6 +137,41 @@ func ParseStr(str string) (verbs []string, err error) {
 	return verbs, err
 }
 
-func RemoveTags(str string) string {
+// var g = &gt.Build{
+// 	Index: .gt.Keys{
+// 		"homepage-greeting": gt.Strings{
+// 			"en": "Welcome to %stitle, %s#name!"
+// 			"es-LA": "¡Bienvenido a %s#title, %s#titlename!",
+// 			"nl": "Welkom bij %s#title, %s#name!",
+// 			"tr": "%s#name, %s#title'ya hoşgeldiniz!", // tag notation proves useful in SOV languages where subject comes before object/verb
+// 			"zh-CN": "欢迎%s#title, #name!", 
+// 		},
+// 	}, 
+// 	Origin: "en",
+// }
 
-}
+// t.Target = "es"
+
+// str := g.T("homepage-greeting")
+
+// t.Target = "nl"
+
+// str = g.T("homepage-greeting")
+// Example:
+//     fmt.Printf(t.S("Welcome to %s#title, %s!#name!"), "gotr", "Melvin")
+//     // outputs: "¡Bienvenido a gotr, Melvin!"
+//     
+// You can add meta data to the printf verbs by using the tag notation. Not only will translators
+// understand more about the context that they're translating in, but it also prevents order issues 
+// in certain languages. (The returning string is always correctly parsed).
+// 
+// Tags are recommended but you can also use regular printf strings. But then the
+// order can be wrong in certain SOV languages (Turkish, Japanese):
+//
+//     fmt.Printf(t.S("Welcome to %s, %s"), "gotr", "Melvin")
+//     // outputs: "gotr, Melvin'ya hoşgeldiniz!" in Turkish. 
+//     // This roughly translates to: "Welcome to Melvin, gotr!" which is not what you want!
+//     
+// Tags are normally ended by a single space but you can also end tags with a plus sign:
+// "somethingbefore%s#name+somethingafter" 
+// 
