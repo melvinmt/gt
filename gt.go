2a8e25cdb8c2cebbc1f59c0360b3a366f3d210f6
Melvin Tercan <mmtercan@gmail.com>
2013-05-30 17:09:54 -0600

    Add synopsis

--- gt.go
+++ gt.go
@@ -16,6 +16,9 @@
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
 // DEALINGS IN THE SOFTWARE.
 
+
+/* A tiny but powerful Go internationalization (i18n) library.
+*/
 package gt
 
 import (
